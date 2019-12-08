4f10f5d9dc8acec13f95989a80ba53b06fcac28c
Karim216 <njigbetnkom@gmail.com>
2019-12-08 08:22:25 -0800

    ajout espace prestataire: Ahmed Karim

---
+++ AhmedKarim/prestataire/src/callbacks.h
@@ -0,0 +1,89 @@
+#include <gtk/gtk.h>
+
+
+void on_buttonConnectAgent_clicked (GtkWidget *obj, gpointer user_data);
+
+void on_buttonQuitAuthentif_clicked (GtkWidget *obj, gpointer user_data);
+
+void on_buttonAuthentification_clicked (GtkWidget *obj, gpointer user_data);
+
+void on_buttonDeconnPrestataire_clicked (GtkWidget *obj, gpointer user_data);
+
+void on_buttonConsultPrestOffre_clicked (GtkWidget *obj, gpointer user_data);
+
+void on_buttonAjouterVol_clicked (GtkWidget *obj, gpointer user_data);
+
+void on_buttonConsultPrestClient_clicked    (GtkWidget *obj, gpointer user_data);
+
+void on_buttonAffPrestations_clicked (GtkWidget *obj, gpointer user_data);
+
+void on_buttonSupprPrestVol_clicked (GtkWidget *obj, gpointer user_data);
+
+void on_buttonVerifModifPrestVol_clicked (GtkWidget *obj, gpointer user_data);
+
+void on_buttonModifierPerstVol_clicked (GtkWidget *obj, gpointer user_data);
+
+void on_buttonRetourPrestEtOffre_clicked (GtkWidget *obj, gpointer user_data);
+
+
+void on_buttonAjoutHebergement_clicked (GtkWidget *obj, gpointer user_data);
+
+void on_buttonPagePrestOffre_clicked (GtkWidget *obj, gpointer user_data);
+
+void on_buttonRetourAccPrest_clicked (GtkWidget *obj, gpointer user_data);
+
+void on_buttonActuaOffresEtPrest_clicked (GtkWidget *obj, gpointer user_data);
+
+void on_buttonValideSupprHbr_clicked (GtkWidget *obj, gpointer user_data);
+
+void on_buttonVerifierIdModifHbr_clicked (GtkWidget *obj, gpointer user_data);
+
+void on_buttonModifHbr_clicked (GtkWidget *obj, gpointer user_data);
+
+void on_buttonAfficherPrestations_clicked (GtkWidget *obj, gpointer user_data);
+
+void on_buttonRafraichirPrestations_clicked (GtkWidget *obj, gpointer user_data);
+
+//ajout d'une prestation pour voitures
+void
+on_buttonAjoutVoiturePrest_clicked     (GtkWidget *obj, gpointer user_data);
+
+void
+on_consulterPrestations_clicked        (GtkWidget       *obj,
+                                        gpointer         user_data);
+
+void
+on_button1_clicked                     (GtkWidget       *obj,
+                                        gpointer         user_data);
+
+void
+on_buttonModifVoiturePrest_clicked     (GtkWidget       *obj,
+                                        gpointer         user_data);
+
+void
+on_buttonSupprVoiturePrest_clicked     (GtkWidget       *obj,
+                                        gpointer         user_data);
+
+void
+on_button5_clicked                     (GtkWidget       *obj,
+                                        gpointer         user_data);
+
+void
+on_button6_clicked                     (GtkWidget       *obj,
+                                        gpointer         user_data);
+
+void
+on_button7_clicked                     (GtkWidget       *obj,
+                                        gpointer         user_data);
+
+void
+on_button8_clicked                     (GtkWidget       *obj,
+                                        gpointer         user_data);
+
+void
+on_buttonAccCatalogue_clicked          (GtkWidget       *obj,
+                                        gpointer         user_data);
+
+void
+on_calendarOffresPrests_day_selected   (GtkCalendar     *calendar,
+                                        gpointer         user_data);
---
+++ AhmedKarim/prestataire/src/verifAuthentif.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "verifAuthentif.h"
+
+
+int verifAuthentification(char user_name[], char pwd[]){
+
+	char us[50], pass[50];
+	int role;
+	FILE *f;
+
+	f=fopen("authentification.txt","r");
+	while (fscanf(f,"%s %s %d", us, pass, &role)!=EOF){
+		if(strcmp(user_name,us)==0 && strcmp(pwd,pass)==0){
+			return role;
+		}
+		else{
+			role=0;
+			return role;
+			
+		}
+		fclose(f);
+		
+	}
+	
+}
