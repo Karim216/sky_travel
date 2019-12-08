2664cc66313d91e43c5e00ad392b9233eec043ec
Karim216 <njigbetnkom@gmail.com>
2019-12-08 13:02:29 -0800

    ajout espace de travail en groupe

---
+++ SkyTravel/integration/src/callbacks.h
@@ -0,0 +1,161 @@
+#include <gtk/gtk.h>
+
+
+void on_buttonConnectAgent_clicked (GtkWidget *obj, gpointer user_data);
+
+void on_buttonQuitAuthentif_clicked (GtkWidget *obj, gpointer user_data);
+
+void on_buttonAuthentification_clicked (GtkWidget *obj, gpointer user_data);
+
+void on_buttonDeconnPrestataire_clicked (GtkWidget *obj, gpointer user_data);
+
+void on_buttonConsultPrestOffre_clicked (GtkWidget *obj, gpointer user_data);
+
+void on_buttonAjouterVol_clicked (GtkWidget *obj, gpointer user_data);
+
+void on_buttonConsultPrestClient_clicked    (GtkWidget *obj, gpointer user_data);
+
+void on_buttonAffPrestations_clicked (GtkWidget *obj, gpointer user_data);
+
+void on_buttonSupprPrestVol_clicked (GtkWidget *obj, gpointer user_data);
+
+void on_buttonVerifModifPrestVol_clicked (GtkWidget *obj, gpointer user_data);
+
+void on_buttonModifierPerstVol_clicked (GtkWidget *obj, gpointer user_data);
+
+void on_buttonRetourPrestEtOffre_clicked (GtkWidget *obj, gpointer user_data);
+
+
+void on_buttonAjoutHebergement_clicked (GtkWidget *obj, gpointer user_data);
+
+void on_buttonPagePrestOffre_clicked (GtkWidget *obj, gpointer user_data);
+
+void on_buttonRetourAccPrest_clicked (GtkWidget *obj, gpointer user_data);
+
+void on_buttonActuaOffresEtPrest_clicked (GtkWidget *obj, gpointer user_data);
+
+void on_buttonValideSupprHbr_clicked (GtkWidget *obj, gpointer user_data);
+
+void on_buttonVerifierIdModifHbr_clicked (GtkWidget *obj, gpointer user_data);
+
+void on_buttonModifHbr_clicked (GtkWidget *obj, gpointer user_data);
+
+void on_buttonAfficherPrestations_clicked (GtkWidget *obj, gpointer user_data);
+
+void on_buttonRafraichirPrestations_clicked (GtkWidget *obj, gpointer user_data);
+
+//ajout d'une prestation pour voitures
+void
+on_buttonAjoutVoiturePrest_clicked     (GtkWidget *obj, gpointer user_data);
+
+void
+on_consulterPrestations_clicked        (GtkWidget       *obj,
+                                        gpointer         user_data);
+
+void
+on_button1_clicked                     (GtkWidget       *obj,
+                                        gpointer         user_data);
+
+void
+on_buttonModifVoiturePrest_clicked     (GtkWidget       *obj,
+                                        gpointer         user_data);
+
+void
+on_buttonSupprVoiturePrest_clicked     (GtkWidget       *obj,
+                                        gpointer         user_data);
+
+void
+on_button5_clicked                     (GtkWidget       *obj,
+                                        gpointer         user_data);
+
+void
+on_button6_clicked                     (GtkWidget       *obj,
+                                        gpointer         user_data);
+
+void
+on_button7_clicked                     (GtkWidget       *obj,
+                                        gpointer         user_data);
+
+void
+on_button8_clicked                     (GtkWidget       *obj,
+                                        gpointer         user_data);
+
+void
+on_buttonAccCatalogue_clicked          (GtkWidget       *obj,
+                                        gpointer         user_data);
+
+void
+on_calendarOffresPrests_day_selected   (GtkCalendar     *calendar,
+                                        gpointer         user_data);
+
+void
+on_button13_clicked                    (GtkWidget       *obj,
+                                        gpointer         user_data);
+
+void
+on_button10_clicked                    (GtkWidget       *obj,
+                                        gpointer         user_data);
+
+void
+on_button14_clicked                    (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_button15_clicked                    (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_button12_clicked                    (GtkWidget       *obj,
+                                        gpointer         user_data);
+
+void
+on_button16_clicked                    (GtkWidget       *obj,
+                                        gpointer         user_data);
+
+void
+on_button22_clicked                    (GtkWidget       *obj,
+                                        gpointer         user_data);
+
+void
+on_button23_clicked                    (GtkWidget       *obj,
+                                        gpointer         user_data);
+
+void
+on_button21_clicked                    (GtkWidget       *obj,
+                                        gpointer         user_data);
+
+void
+on_button17_clicked                    (GtkWidget       *obj,
+                                        gpointer         user_data);
+
+void
+on_button24_clicked                    (GtkWidget       *obj,
+                                        gpointer         user_data);
+
+void
+on_button18_clicked                    (GtkWidget       *obj,
+                                        gpointer         user_data);
+
+void
+on_button25_clicked                    (GtkWidget       *obj,
+                                        gpointer         user_data);
+
+void
+on_button26_clicked                    (GtkWidget       *obj,
+                                        gpointer         user_data);
+
+void
+on_button27_clicked                    (GtkWidget       *obj,
+                                        gpointer         user_data);
+
+void
+on_button19_clicked                    (GtkWidget       *obj,
+                                        gpointer         user_data);
+
+void
+on_button29_clicked                    (GtkWidget       *obj,
+                                        gpointer         user_data);
+
+void
+on_button28_clicked                    (GtkWidget       *obj,
+                                        gpointer         user_data);
